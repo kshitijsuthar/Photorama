8c592aab75ecfed3a447fd3f25a62b3093f8f93c
Kshitij Suthar <kshitijsuthar@kshitijs-macbook-pro.local>
2018-03-12 19:26:08 -0400

    Created PhotosViewController

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Kshitij Suthar on 2018-03-12.
+//  Copyright © 2018 Kshitij Suthar. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageview: UIImageView!
+    
+}
