40c6cce8d214137c9104eac089cccdf226381d51
Kshitij Suthar <kshitijsuthar@kshitijs-macbook-pro.local>
2018-03-12 21:10:42 -0400

    Project Working

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+enum FlickrError: Error {
+    case invalidJSONData
+}
+
 enum Method: String {
     case interestingPhotos = "flickr.interestingness.getList"
 }
@@ -17,7 +21,13 @@ struct FlickAPI {
     private static let baseURLString = "https://api.flickr.com/services/rest"
     private static let apiKey = "3a7524bab72d81e35c6f5f4d20f177e4"
     
-    private func flickrURL(method: Method, parameters: [String:String]?) -> URL {
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
+    
+    private static func flickrURL(method: Method, parameters: [String:String]?) -> URL {
         
         var components = URLComponents(string: FlickAPI.baseURLString)!
         
@@ -54,18 +64,50 @@ struct FlickAPI {
         do{
             let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
             
+            guard
+            let jsonDictionary = jsonObject as? [AnyHashable: Any],
+            let photos = jsonDictionary["photos"] as? [String: Any],
+            let photosArray = photos["photo"] as? [[String:Any]]
+                else {
+                    return .failure(FlickrError.invalidJSONData)
+            }
+            
             var finalPhotos = [Photo]()
+            for photoJSON in photosArray {
+                if let photo = photo(fromJSON: photoJSON) {
+                    finalPhotos.append(photo)
+                }
+            }
+            
+            if finalPhotos.isEmpty && !photosArray.isEmpty {
+                return .failure(FlickrError.invalidJSONData)
+            }
             return .success(finalPhotos)
         } catch let error {
             return .failure(error)
         }
     }
     
-    var interestingPhotosURL: URL {
+    private static func photo(fromJSON json: [String: Any]) -> Photo? {
+        guard
+            let photoID = json["id"] as? String,
+            let title = json["title"] as? String,
+            let dateString = json["datetaken"] as? String,
+            let photoURLString = json["url_h"] as? String,
+            let url = URL(string: photoURLString),
+            let dateTaken = dateFormatter.date(from: dateString) else {
+                return nil
+        }
+        return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
+    }
+    
+    static var interestingPhotosURL: URL {
         return flickrURL(method: .interestingPhotos, parameters: ["extras": "url_h,date_taken"])
     }
    
 }
+
+
 
 
 
--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotosResult {
     case success([Photo])
@@ -21,31 +31,75 @@ class PhotoStore{
     }()
     
     
-    func fetchInterestingPhotos(){
+    func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void){
         let url = FlickAPI.interestingPhotosURL
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request){
             (data, response, error) -> Void in
             
-            if let jsonData = data {
-                do {
-                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
-                    print(jsonObject)
-                }
-                
-                catch let error {
-                    print("Error creating json object: \(error)")
-                }
+            let result = self.processPhotoRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
             }
-            else if let requestError = error {
-                print("Error Fetching interesting photos: \(requestError)")
-            }
-            else {
-                print("Unexpected error with the request")
-            }
+            completion(result)
+//            if let jsonData = data {
+//                do {
+//                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+//                    print(jsonObject)
+//                }
+//
+//                catch let error {
+//                    print("Error creating json object: \(error)")
+//                }
+//            }
+//            else if let requestError = error {
+//                print("Error Fetching interesting photos: \(requestError)")
+//            }
+//            else {
+//                print("Unexpected error with the request")
+//            }
         }
         
         task.resume()
+    }
+    
+    private func processPhotoRequest(data: Data?, error: Error?) -> PhotosResult {
+        guard let jsonData = data else {
+            return .failure(error!)
+        }
+        return FlickAPI.photos(fromJSON: jsonData)
+    }
+    
+    func fetchImage(for photo:Photo, completion: @escaping (ImageResult) -> Void){
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+            completion(result)
+        }
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                
+                if data == nil {
+                    return .failure(error!)
+                }
+                else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        
+        return .success(image)
     }
     
 }
